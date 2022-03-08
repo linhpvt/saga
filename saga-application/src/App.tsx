908791f049cf470555876c66aee422c52180cf3f
linhpvt <linhpvt60@gmail.com>
2022-03-08 11:38:36 +0700

    add .gitattributes file

--- saga-application/src/App.tsx
+++ saga-application/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { toString, FORMAT } from './helpers/datetime';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit {toString(new Date(), FORMAT.Y_M_D, 'ERROR')} <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
---
+++ saga-application/src/helpers/datetime.ts
@@ -0,0 +1,19 @@
+export const FORMAT = {
+  YMD: 'yyyy/MM/dd',
+  Y_M_D: 'yyyy-MM-dd',
+}
+export const toString = (date: Date, mask: string, fallbackValue: any) => {
+  const y = date.getFullYear();
+  let m = `${date.getMonth() + 1}`;
+  let d = `${date.getDate()}`;
+  m = m.length > 1 ? m : `0${m}`;
+  d = d.length > 1 ? d : `0${d}`;
+  switch (mask) {
+    case (FORMAT.YMD):
+      return `${y}/${m}/${d}`;
+    case (FORMAT.Y_M_D):
+      return `${y}-${m}-${d}`    
+    default:
+      return fallbackValue;
+  }
+}
